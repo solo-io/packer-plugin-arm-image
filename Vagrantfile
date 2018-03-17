solo-io/packer-plugin-arm-image

commit e32d99b53424e068648d31392632949f3f7ec4dd

Use the generic/ubuntu1604 image.
Compared to the custom "velocity42/xenial64" image, the build process
for "generic/ubuntu1604" is well-documented and reproducible and the
image is regularly updated to match latest security packages and updates
from Ubuntu.
Furthermore, the image is available for more providers, including libvirt
and Hyper-V.

Vagrantfile
@@ -2,6 +2,6 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "velocity42/xenial64"
+  config.vm.box = "generic/ubuntu1604"
   config.vm.provision "shell", :path => "provision.sh", privileged: false
 end

commit 2738b03a61f15a0790f3781ceb3df9373139f69d

[PATCH] Fix /vagrant synced folder
e32d99b5 introduced a regression because generic/ubuntu1604 doesnot have
the default /vagrant synced folder. This patch re-enables it and additionally
uses ubuntu/xenial64 for virtualbox builds because generic/ubuntu1604 does
not come with virtualbox-guest-utils installed.

Vagrantfile
@@ -3,5 +3,15 @@
 
 Vagrant.configure("2") do |config|
   config.vm.box = "generic/ubuntu1604"
+  config.vm.provider :virtualbox do |vb, override|
+    # generic/ubuntu1604 does not come with virtualbox-guest-utils installed even
+    # though a virtualbox flavor exists.
+    # Therefore, override the image for virtualbox provider.
+    override.vm.box = "ubuntu/xenial64"
+    # disable the generation of ubuntu-xenial-16.04-cloudimg-console.log file
+    # https://betacloud.io/get-rid-of-ubuntu-xenial-16-04-cloudimg-console-log/
+    vb.customize [ "modifyvm", :id, "--uartmode1", "disconnected" ]
+  end
+  config.vm.synced_folder "./", "/vagrant", disabled: false
   config.vm.provision "shell", :path => "provision.sh", privileged: false
 end