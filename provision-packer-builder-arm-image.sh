162b418b6276c5cb49dfd86ce35962cf9aaa79e9
Thomas Liebetraut <thomas@tommie-lie.de>
2018-03-19 16:22:44 +0100

    Split provisioning script into three parts: build-env, packer-builder-arm-image and build-image
    Splitting up the provisioners facilitates the re-use of the Vagrant box
    for building images in a clean-room environment. Images can be rebuild
    using
      $ vagrant provision --provision-with build-image
    once the packer json definition file has been changed. This step
    does not require all the upgrading and compiling steps to be re-done.
    By default, all provisioning parts are done in order, i.e. just doing a
      $ vagrant up
    will prepare the build environment, compile the packer plugin and build the
    packer image.

---
+++ provision-packer-builder-arm-image.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# @script          provision.sh
+# @description     provisioning script that builds environment for
+#                  https://github.com/solo-io/packer-builder-arm-image
+#
+#                 By default, sets up environment, builds the plugin, and image
+##
+set -x
+# Set to false to disable auto building
+
+# Now ready to build the plugin
+mkdir -p $GOPATH/src/github.com/solo-io/
+pushd $GOPATH/src/github.com/solo-io/
+# clean up potential residual files from previous builds
+rm -rf packer-builder-arm-image
+if [[ -z "${GIT_CLONE_URL}" ]]; then {
+    cp -a /vagrant packer-builder-arm-image
+} else {
+    git clone ${GIT_CLONE_URL} packer-builder-arm-image
+}; fi
+pushd ./packer-builder-arm-image
+dep ensure
+go build
+
+# Check if plugin built and copy into place
+if [[ ! -f packer-builder-arm-image ]]; then {
+    echo "Error Plugin failed to build."
+    exit
+} else {
+    cp packer-builder-arm-image /vagrant
+}; fi
+
