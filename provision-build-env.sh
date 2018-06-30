dd68a779cac9cd0812c2e68c0f57f5de6d5ffdd2
Raphael Randschau <nicolai86@me.com>
2018-06-29 19:34:52 -0700

    ensure no packer file exists during provisioning
    this allows to update packer by running `vagrant provision`

--- provision-build-env.sh
+++ provision-build-env.sh
@@ -45,6 +45,7 @@ export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 go get -u github.com/golang/dep/cmd/dep
 
 # Download and install packer
+[[ -e /tmp/packer ]] && rm /tmp/packer
 wget https://releases.hashicorp.com/packer/1.2.4/packer_1.2.4_linux_amd64.zip \
     -q -O /tmp/packer_1.2.4_linux_amd64.zip
 pushd /tmp
