421b6bbc9b747de575c92f086ba7a3685c57d30b
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-11-30 10:57:53 -0500

    update scripts

--- hack/upload.sh
+++ hack/upload.sh
@@ -4,6 +4,9 @@ if [ -z "$TAG_NAME" ]; then
     exit 0
 fi
 
+go build
+(cd cmd/flasher; go build)
+
 ./hack/github-release.sh owner=solo-io repo=packer-builder-arm-image tag=$TAG_NAME
 ./hack/upload-github-release-asset.sh owner=solo-io repo=packer-builder-arm-image tag=$TAG_NAME filename=./packer-builder-arm-image
 ./hack/upload-github-release-asset.sh owner=solo-io repo=packer-builder-arm-image tag=$TAG_NAME filename=./cmd/flasher/flasher
