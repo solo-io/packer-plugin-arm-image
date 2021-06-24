794fd7a2d0dc3878db9ad44769d02cde492e1107
Blake Covarrubias <blake@covarrubi.as>
2021-06-23 21:59:31 -0700

    Update Packer to 1.7.3

--- provision-build-env.sh
+++ provision-build-env.sh
@@ -59,10 +59,10 @@ export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
 # Download and install packer
 [[ -e /tmp/packer ]] && rm -rf /tmp/packer*
-wget https://releases.hashicorp.com/packer/1.5.2/packer_1.5.2_linux_amd64.zip \
-    -q -O /tmp/packer_1.5.2_linux_amd64.zip
+wget https://releases.hashicorp.com/packer/1.7.3/packer_1.7.3_linux_amd64.zip \
+    -q -O /tmp/packer_1.7.3_linux_amd64.zip
 cd /tmp
-unzip -u packer_1.5.2_linux_amd64.zip
+unzip -u packer_1.7.3_linux_amd64.zip
 sudo cp packer /usr/local/bin
 rm -rf /tmp/packer*
 cd ..
