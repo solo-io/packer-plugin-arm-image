cb258fd9f02b9474d1a7f8fb72092100921716cc
binhong.hua <binhong.hua@gmail.com>
2022-05-10 21:05:05 +0800

    Add timeout for loopup /dev/loop device
    sometime /dev/loop device not ready when losetup success.

--- pkg/builder/step_map_image.go
+++ pkg/builder/step_map_image.go
@@ -9,6 +9,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/hashicorp/packer-plugin-sdk/multistep"
 	"github.com/hashicorp/packer-plugin-sdk/packer"
@@ -45,28 +46,57 @@ func (s *stepMapImage) Run(_ context.Context, state multistep.StateBag) multiste
 	}
 	path := strings.TrimSpace(string(out))
 	loop := strings.Split(path, "/")[2]
+	prefix := loop + "p"
 
 	// Look for all partitions of created loopback
 	var partitions []string
-	files, err := os.ReadDir("/dev/")
-	if err != nil {
-		ui.Error("Couldn't list devices in /dev/")
-		s.Cleanup(state)
-		return multistep.ActionHalt
-	}
-	prefix := loop + "p"
-	for _, file := range files {
-		if strings.HasPrefix(file.Name(), prefix) {
-			partitions = append(partitions, "/dev/"+file.Name())
+	cPartitions := make(chan []string)
+	action := make(chan multistep.StepAction)
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		for {
+			select {
+			default:
+				files, err := os.ReadDir("/dev/")
+				if err != nil {
+					ui.Error("Couldn't list devices in /dev/")
+					s.Cleanup(state)
+					action <- multistep.ActionHalt
+					return
+				}
+
+				for _, file := range files {
+					if strings.HasPrefix(file.Name(), prefix) {
+						partitions = append(partitions, "/dev/"+file.Name())
+					}
+				}
+
+				if len(partitions) == 0 {
+					continue
+				} else {
+					cPartitions <- partitions
+					return
+				}
+			case <-ctx.Done():
+				return
+			}
 		}
+	}()
+
+	select {
+	case action := <-action:
+		return action
+	case partitions = <-cPartitions:
+		partPrefix := "/dev/" + prefix
+		// sort by files by partition number, to make sure they match the partition map.
+		sort.Slice(partitions, func(i, j int) bool {
+			n_i, _ := strconv.Atoi(partitions[i][len(partPrefix):])
+			n_j, _ := strconv.Atoi(partitions[j][len(partPrefix):])
+			return n_i < n_j
+		})
+	case <-time.After(time.Second):
+		cancel()
 	}
-	partPrefix := "/dev/" + prefix
-	// sort by files by partition number, to make sure they match the partition map.
-	sort.Slice(partitions, func(i, j int) bool {
-		n_i, _ := strconv.Atoi(partitions[i][len(partPrefix):])
-		n_j, _ := strconv.Atoi(partitions[j][len(partPrefix):])
-		return n_i < n_j
-	})
 
 	state.Put(s.ResultKey, partitions)
 
